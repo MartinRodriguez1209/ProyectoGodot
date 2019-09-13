1fd095fd80670c7fd2fd09bf5be24b6d55710b76
MartinRodriguez1209 <martinrodriguez.1209@gmail.com>
2019-09-12 21:33:32 -0300

    menu

---
+++ ProyectAssets/TestAsset/Main Menu.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$MarginContainer/VBoxContainer/VBoxContainer/TextureButton.grab_click_focus()
+
+func _process(delta):
+	if $MarginContainer/VBoxContainer/VBoxContainer/TextureButton.is_hovered() == true:
+		$MarginContainer/VBoxContainer/VBoxContainer/TextureButton.grab_focus()
+	if $MarginContainer/VBoxContainer/VBoxContainer/TextureButton2.is_hovered() == true:
+		$MarginContainer/VBoxContainer/VBoxContainer/TextureButton2.grab_focus()
